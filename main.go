9eca566b8c6a3e38131ee8e2d2e5ece2569c9be3
shubashdutta <141384565+shubashdutta@users.noreply.github.com>
2023-08-16 21:08:33 +0545

    Update main.go

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 
-	"github.com/rs/cors"
-	"github.com/shubash/saibaba/router"
+    "github.com/rs/cors"
+    "github.com/shubash/saibaba/router"
 )
+
 func setupCors() *cors.Cors {
-	return cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://shuas.onrender.com""}, // Replace with your frontend's URL
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Authorization", "Content-Type"},
-		AllowCredentials: true,
-	})
+    return cors.New(cors.Options{
+        AllowedOrigins:   []string{"https://saibabasevasadantrust.com"}, // Your frontend's HTTPS URL
+        AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowedHeaders:   []string{"Authorization", "Content-Type"},
+        AllowCredentials: true,
+    })
 }
+
 func main() {
-	r := router.Router()
+    r := router.Router()
 
-	// Set up CORS middleware
-	c := setupCors()
-	handler := c.Handler(r)
+    // Set up CORS middleware
+    c := setupCors()
+    handler := c.Handler(r)
 
-	port := "8081" // Change the port to your desired value
+    port := "8081" // Change the port to your desired value
 
-	fmt.Printf("Server is ready and running on port %s...\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, handler))
+    fmt.Printf("Server is ready and running on port %s...\n", port)
+    log.Fatal(http.ListenAndServe(":"+port, handler))
 }
-
